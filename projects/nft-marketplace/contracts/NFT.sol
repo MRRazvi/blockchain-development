d8e8664930df0d2171e6720d22aa81c3ecde87e4
Rizi More <rizimore@gmail.com>
2022-01-07 22:40:40 +0500

    nft contract

--- projects/nft-marketplace/contracts/NFT.sol
+++ projects/nft-marketplace/contracts/NFT.sol
@@ -1,1 +1,26 @@
-//
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import '@openzeppelin/contracts/utils/Counters.sol';
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import '@openzeppelin/contracts/token/ERC721/ERC721Metadata.sol';
+import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
+
+contract NFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    address contractAddress;
+
+    constructor(address marketplaceAddress) ERC721('TecHouse', 'TH') {
+        contractAddress = marketplaceAddress;
+    }
+
+    function createToken(string memory tokenURI) public returns (uint) {
+        _tokenIds.increment();
+        uint256 tokenId = _tokenIds.current();
+        _mint(msg.sender, tokenId);
+        _setTokenURI(tokenId, tokenURI);
+        setApprovalForAll(contractAddress, true);
+        return tokenId;
+    }
+}
